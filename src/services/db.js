a3008e0f748056d75ce21f7ab9e0441e726f775b
Husankhon Bahodirkhonov <wiut2024id17965@gmail.com>
2025-04-04 11:51:16 +0500

    Remove mongo ca certificate

--- src/services/db.js
+++ src/services/db.js
@@ -3,13 +3,11 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
   try {
     const MONGODB_URI = process.env.MONGODB_URI || '';
-    const MONGODB_CA_PATH = process.env.MONGODB_CA_PATH || '';
     
     const conn = await mongoose.connect(MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       serverSelectionTimeoutMS: 5000,
-      tlsCertificateKeyFile: MONGODB_CA_PATH,
     });
     
     console.log(`MongoDB Connected: ${conn.connection.host}`);
